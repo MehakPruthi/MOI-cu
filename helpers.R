6faf0ba84acd0fa5042d546cdb9fb8604514cc00
Paul Deng <puyuan.deng@wsp.com>
2020-02-16 15:12:54 -0500

    otg threshold should be passed as an argument.

--- helpers.R
+++ helpers.R
@@ -33,7 +33,7 @@ haverFunctionEuclidean <- function(lat1, lon1, lat2, lon2) {
   #' @param lat2 Latitude value of the second point
   #' @param lon2 Longitude value of the second point
   #' @return Euclidean distance in KM between the two points
-
+  
   earthRadius <- 6378.1 #in km
   lat1Rad <- convertDegrees(lat1)
   lat2Rad <- convertDegrees(lat2)
@@ -57,7 +57,7 @@ haverFunctionManhattan <- function(lat1, lon1, lat2, lon2) {
   #' @param lat2 Latitude value of the second point
   #' @param lon2 Longitude value of the second point
   #' @return Manhattan distance in KM between the two points
-
+  
   # Determine 'corner' point in Pythagorean triangle by assiging lat, lon from other points
   lat3 <- lat1
   lon3 <- lon2
@@ -91,7 +91,7 @@ sample_by_row <- function(row) {
   
   x <- row["sfis.list"][[1]]
   size <- row["enrolment.rounded"][[1]]
-
+  
   if (length(x) == 1) {
     # Quirk 
     prob = c(rep(0, x - 1), row["school.weight.prob.list"][[1]])
@@ -108,7 +108,7 @@ balance <- function(matrix, tot, axis) {
   #' @param tot The vector to be balanced against
   #' @param axis The direction of the balance, 1 indicates rows, 2 indicates columns
   #' @return A balanced matrix
-
+  
   if (axis == 1) {
     sum <- rowSums(matrix)
   } else if (axis == 2) {
@@ -129,7 +129,7 @@ calc_error <- function(matrix, a, b) {
   #' @param a The row vector
   #' @param b The column vector
   #' @return The total difference between the row sum of the matrix and the row vector and the column sum of the matrix and column vector
-
+  
   row_sum <- sum(abs(a - rowSums(matrix)))
   col_sum <- sum(abs(b - colSums(matrix)))
   return(row_sum + col_sum)
@@ -210,7 +210,7 @@ matrix_balancing_2d <- function(matrix, a, b, totals_to_use="raise", max_iterati
   #' @param max_iterations The maximum number of iterations this procedure should run for
   #' @param rel_error The stopping threshold for this procedure
   #' @return Two dimentionally balanced matrix
-
+  
   valid_totals_to_use = c("rows", "columns", "average", "raise")
   if (!(totals_to_use %in% valid_totals_to_use)) {
     stop("totals_to_use is invalid, not one of ('rows', 'columns', 'average', 'raise')")
@@ -362,7 +362,7 @@ trip_percentile <- function(trip_df, calc_type="time", percentile) {
   #' @param calc_type A string of either 'time' or 'distance'
   #' @param percentile A double that represent the percentile the user wish to calculate
   #' @return The percentile value
-
+  
   if (tolower(calc_type) == 'time') {
     percentileVal <- trip_df %>% 
       ungroup() %>%
@@ -454,7 +454,7 @@ plot_straight_line_tlfd <- function(tlfd) {
 
 plot_travel_time_tlfd <- function(tlfd) {
   #' Plot TLFD with segmentation
-
+  
   g1 <- ggplot(tlfd, aes(travel.time, color = as.factor(board_type_name))) +
     geom_freqpoly(stat = 'bin', binwidth = 2, size = 1) + 
     facet_wrap(vars(board_type_name), scales = 'free_y') +
@@ -588,7 +588,7 @@ create_student_xy <- function(student_travel, treso_shp) {
   #' @param student_travel A Dataframe of the students and schools within a certain catchment distance of the school
   #' @param treso_shp A Shapefile of TRESO zones
   #' @return A SpatialPointsDataframe 
-
+  
   student_spdf <- student_travel %>%
     ungroup() %>%
     select(student.lat, student.long, dist, man.dist, school.name, sfis, dsb.index, panel, enrolment, student.postal.code) %>%
@@ -694,7 +694,7 @@ summarize_buffered_zones <- function(buffered_df, treso_tb, school_ade, school_b
   #' @param school_board_def A Dataframe with the school board definitions
   #' @param treso_zone_def A Dataframe with the TRESO zone definitions
   #' @return A Dataframe of the summarized socio-economic information for each school
-
+  
   # First summarise data that is calculated with `mean()` or `first()`
   # I think using purr, one could summarise different columns with different functions in one go
   school_tb_temp <- as_tibble(buffered_df) %>%
@@ -779,7 +779,7 @@ construction_cost <- function(user_input_inflation, user_input_scenario_year, cu
   #' @param current_year An integer that represents the current year upon which construction timelines are set
   #' @param new_facility_list A list of new facilities to be built
   #' @return A list of spending in nominal dollars per year, and in total, to build the new facilities
-
+  
   # Determine min and max years for building inflation index
   min_year = min(user_input_scenario_year, current_year) 
   max_year = max(user_input_scenario_year, current_year)
@@ -823,7 +823,7 @@ create_pos_vector <- function(school_df, new_school, full_vector) {
   #' @param full_vector The full list of TRESO zones in order
   #' @return a data matrix
   #' 
-
+  
   # Include the new_school in the master school list
   if(!is.null(new_school)){
     school_df <- school_df %>% 
@@ -839,7 +839,7 @@ create_pos_vector <- function(school_df, new_school, full_vector) {
     arrange(treso.id.pos) %>%
     column_to_rownames(var = "treso.id.pos") %>%
     data.matrix()
-
+  
   return(pos)
 }
 
@@ -905,7 +905,7 @@ calculate_school_weight_forecasting <- function(trip_list, school_df, eqao_2017,
     school_df <- school_df %>% 
       bind_rows(new_school)
   }
-
+  
   # Calculate school weighting for TRESO zones with multiple schools and export it for
   pos_school <- trip_list %>%
     group_by(treso.id.pos) %>%
@@ -1002,14 +1002,15 @@ distribute_students_within_zones <- function(school_forecast_df) {
   return(school_forecast_distributed_df)
 }
 
-create_forecast_school_list <- function(school_base, new_school_df, school_diff) {
-
+create_forecast_school_list <- function(school_base, new_school_df, school_diff, otg_threshold) {
+  
   #' Create the forecasted school list with OTG, OTG Threshold, ADE and Simulated ADE
   #' Also redistribute any overfilled schools in a zone to 
   #' 
   #' @param school_base
   #' @param new_school_df
   #' @param school_diff
+  #' @param otg_threshold
   #' @return A Dataframe of schools with forecasted/simulated ADE and 
   
   school_forecast_df <- school_base %>% 
@@ -1027,7 +1028,7 @@ create_forecast_school_list <- function(school_base, new_school_df, school_diff)
     # However, in test runs, this did not occur at any schools, so risk appears low. If it were to happen, the result would be a slight increase in the total number of ADE across the province, which would almost certainly be negligible. 
     mutate(simulated.ade = ifelse(simulated.ade < 0, 0, simulated.ade)) %>%
     # Create OTG threshold based on user input
-    mutate(otg.threshold = otg * USER_OTG_THRESHOLD) %>% 
+    mutate(otg.threshold = otg * otg_threshold) %>% 
     select(treso.id.pos, sfis, school.name, otg, otg.threshold, ade, simulated.ade)
   
   # Redistribute students from overfilled schools to underfilled schools in the same zone
@@ -1035,7 +1036,7 @@ create_forecast_school_list <- function(school_base, new_school_df, school_diff)
   
   return(school_forecast_distributed_df)
 }
-  
+
 forecast_school_ade <- function(prop_matrix, trip_list, school_df, eqao_2017, new_school) {
   #' Produce a Dataframe with the summary of the school's forecasted ADE
   #'
@@ -1115,7 +1116,7 @@ edu_dm <- function(treso_travel_time, trip_list, treso_zone_def, por_additional,
     rename(travel.time = value) %>%
     left_join(select(treso_zone_def, treso_id, treso.por.csduid = csduid, treso.por.cduid = cduid), by = c('treso.id.por' = 'treso_id')) %>% 
     left_join(select(treso_zone_def, treso_id, treso.pos.csduid = csduid, treso.pos.cduid = cduid), by = c('treso.id.pos' = 'treso_id')) 
-
+  
   # Calculate mean travel time across CD and filter out the shorter trips
   threshold_tt_zones <- potential_zones %>%
     group_by(treso.por.cduid) %>%
@@ -1133,7 +1134,7 @@ edu_dm <- function(treso_travel_time, trip_list, treso_zone_def, por_additional,
   # Calculate zone pairs for consideration due to school overfill
   additional_pairs <- threshold_tt_zones %>%
     right_join(select(por_additional, treso.id.por), by = c('treso.id.por'))
-
+  
   # Determine shortlist of zones within which construction should be considered by ruling out zones too close to superior zones
   shortlist_zones <- threshold_tt_zones %>%
     # Cut down list of zones under consideration based on minimum bar for 'excess' travel time
@@ -1141,15 +1142,15 @@ edu_dm <- function(treso_travel_time, trip_list, treso_zone_def, por_additional,
     select(treso.id.por) %>% 
     rbind(select(por_additional, treso.id.por)) %>%
     distinct(treso.id.por)
-
+  
   # Retain the original copy of the shortlisted zones
   shortlist_zones_dup <- shortlist_zones
-
+  
   shortlist_pairs <- potential_zones %>%
     select(treso.id.por, treso.id.pos, travel.time) %>%
     inner_join(shortlist_zones, by = c('treso.id.por')) %>%
     inner_join(shortlist_zones, by = c('treso.id.pos' = 'treso.id.por'))
-
+  
   print(paste0("The number of zones in shortlist is: ", nrow(shortlist_zones)))
   print(paste0("The number of zone pairs in shortlist is: ", nrow(shortlist_pairs)))
   
@@ -1157,33 +1158,33 @@ edu_dm <- function(treso_travel_time, trip_list, treso_zone_def, por_additional,
   num_shortlist_zones <- nrow(shortlist_zones)
   build_df <- tibble(zone = integer())
   proximity_df <- tibble(zone = integer())
-
+  
   start.time <- Sys.time()
   print(Sys.time())
-
+  
   # Loop through the shortlisted TRESO locations to choose best zones in which to build
   while (num_shortlist_zones > 0) {
     # Append the highest prioirty TRESO zone to the build_df
     build_zone_id = first(shortlist_zones$treso.id.por)
     build_df <- build_df %>%
       add_row(., zone = build_zone_id)
-  
+    
     print(paste0('Build Zone: ', build_zone_id))
-
+    
     # Create a list of zones to anti_join with shortlist_zones to remove close proximity zones
     origin_zones_removed <- shortlist_pairs %>%
       filter(treso.id.por != build_zone_id) %>%
       filter(treso.id.pos == build_zone_id) %>%
       filter(travel.time <= zone_proximity_threshold) %>%
       distinct(treso.id.por)
-
+    
     # Remove TRESO zones that have been selected or too close to the selected from shortlist_zones
     shortlist_zones <- shortlist_zones %>%
       filter(treso.id.por != build_zone_id) %>%
       anti_join(origin_zones_removed, by = c('treso.id.por'))
-
+    
     print(paste0("The number of zones in shortlist after proximity check is: ", nrow(shortlist_zones)))
-
+    
     # Reduce short_list pairs to exlude previously eliminated zones from future consideration
     shortlist_pairs <- shortlist_pairs %>%
       # Keep OD pairs where the origin zone is not the same as build_zone
@@ -1191,9 +1192,9 @@ edu_dm <- function(treso_travel_time, trip_list, treso_zone_def, por_additional,
       # Keep OD pairs where the destination zone is not the same as build_zone 
       # OR pairs where the destination is the same as build_zone BUT outside of the proximity threshold
       filter(treso.id.pos != build_zone_id | (treso.id.pos == build_zone_id & travel.time > zone_proximity_threshold))
-
+    
     print(paste0('The number of OD pairs in shortlist after proximity check is: ', nrow(shortlist_pairs)))
-
+    
     # Update the number of zones in shortlist_zones
     num_shortlist_zones <- nrow(shortlist_zones)
   }
@@ -1205,12 +1206,13 @@ edu_dm <- function(treso_travel_time, trip_list, treso_zone_def, por_additional,
   proximity_df <- shortlist_zones_dup %>%
     anti_join(build_df, by = c('treso.id.por' = 'zone')) %>%
     distinct(treso.id.por)
-
+  
   return(list(build_df, proximity_df))
 }
 
-calculate_delta_between_simulated_scenarios <- function(school_base, school_summary_base, school_summary_forecast, new_school) {
-
+calculate_delta_between_simulated_scenarios <- function(school_base, school_summary_base, school_summary_forecast, new_school,
+                                                        user_otg_threshold) {
+  
   #' Calculate the delta between the two simulated scenarios. Apply this delta to the `school_base` Dataframe in order to obtain the
   #' forecasted ADE of the schools.
   #'
@@ -1218,6 +1220,7 @@ calculate_delta_between_simulated_scenarios <- function(school_base, school_summ
   #' @param school_summary_base A Dataframe of simulated ADE for schools in the base scenario
   #' @param school_summary_forecast A Dataframe of simulated ADE for schools in the forecast scenario
   #' @param new_school A Dataframe of new schools
+  #' @param user_otg_threshold A numeric value set by the user
   #' @return A Dataframe of schools with the forecasted ADE
   
   # Take the difference between the simulated ADEs
@@ -1226,18 +1229,18 @@ calculate_delta_between_simulated_scenarios <- function(school_base, school_summ
     mutate(change.ade = simulated.ade.20xx - simulated.ade.base)
   
   # Create the forecasted school list with OTG, OTG Threshold, ADE and Simulated ADE
-  school_forecast <- create_forecast_school_list(school_base, new_school, trip_list_schools_summary_difference)
+  school_forecast <- create_forecast_school_list(school_base, new_school, trip_list_schools_summary_difference, user_otg_threshold)
   
   return(school_forecast)
 }
 
 distribution_model <- function(school_base, school_20xx, school_summary_2017, school_summary_20xx, prop_matrix,
                                prop_matrix_balanced, new_school, pos, pos_full,
-                               overfill_threshold = 100, capacity_constrained = FALSE) {
+                               overfill_threshold = 100, user_otg_threshold, capacity_constrained = FALSE) {
   
   # Calculate the delta between simulated base and forecast scenarios and apply to the base scenario
   school_forecast <- calculate_delta_between_simulated_scenarios(school_base, school_summary_2017, school_summary_20xx,
-                                                                 new_school)
+                                                                 new_school, user_otg_threshold)
   
   if (capacity_constrained) {
     overfill_ade <- 999999
@@ -1341,11 +1344,12 @@ distribution_model <- function(school_base, school_20xx, school_summary_2017, sc
         select(-overfill.ade)
       
       # Calculate the delta between simulated base and forecast scenarios and apply to the base scenario
-      school_forecast <- calculate_delta_between_simulated_scenarios(school_base, school_summary_2017, school_summary_20xx, new_school)
+      school_forecast <- calculate_delta_between_simulated_scenarios(school_base, school_summary_2017, school_summary_20xx, new_school,
+                                                                     user_otg_threshold)
       
       i <- i + 1
     }
-
+    
   } else {
     print(paste0("Running distribution capacity unconstrained"))
     # Calculate the delta between simulated base and forecast scenarios and apply to the base scenario
@@ -1365,7 +1369,7 @@ create_hospital_xy <- function(hospital_master) {
   #' @param hospital_master A Dataframe of the hospitals
   #' @param treso_shp A Shapefile of TRESO zones
   #' @return A SpatialPointsDataframe 
-
+  
   hospital_spdf <- hospital_master %>%
     ungroup() %>%
     select(id, hospital.lat, hospital.long) %>%
